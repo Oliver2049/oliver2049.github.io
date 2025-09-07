a54cc50db5c7470a22308119dbb3266b503fa68a
Oliver <minhtien2049@gmail.com>
2025-09-08 01:23:54 +1000

    avatar

--- data/aboutMeData.ts
+++ data/aboutMeData.ts
@@ -4,7 +4,7 @@ const aboutMeData: AboutMeData = {
   name: 'Oliver',
   occupation: 'Security Analyst',
   company: '',
-  avatar: '/public/avatar.jpg',
+  avatar: '/avatar.jpg',
   bio: "Hello! I'm Oliver -  a dedicated passionate security analyst with a strong passion for penetration testing and system hardening. My goal is to study and analyse cyber threats and mitigate them through hands-on security testing, and threat analysis.",
   skills: [
     'Identity and Access Management',
