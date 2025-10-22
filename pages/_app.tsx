5b91df12bb567d218957a31372998b21108aa3b0
Oliver <minhtien2049@gmail.com>
2025-10-22 17:23:37 +1100

    Fix Website error that disallow website information for be retrtieved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,23 +35,8 @@ export default function App({ Component, pageProps }: AppProps): React.ReactNode
   return (
     <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light" enableSystem={false}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-        <title>Oliver's Blog</title>
-        <meta
-          name="description"
-          content="Welcome to Oliver's Cyber Station, a blog about cybersecurity, penetration testing, and SOC analysis."
-        />
-        <meta property="og:title" content="Oliver's Blog" />
-        <meta
-          property="og:description"
-          content="Welcome to Oliver's Cyber Station, a blog about cybersecurity, penetration testing, and SOC analysis."
-        />
-        <meta
-          property="og:image"
-          content="https://oliver2049-github-io.pages.dev/static/images/default-preview.png"
-        />
-        <meta property="og:url" content="https://oliver2049-github-io.pages.dev" />
-        <meta property="og:type" content="website" />
+        <meta content="width=device-width, initial-scale=1.0, viewport-fit=cover" name="viewport" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <div className="font-sans">
